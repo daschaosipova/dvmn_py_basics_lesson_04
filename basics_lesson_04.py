a3ba9c76a69ad54cf23d9b3186a7558f0c8f297c
daschaosipova <daschaosipova@yandex.com>
2025-12-25 10:41:12 +0300

    Переименовала переменные

--- basics_lesson_04.py
+++ basics_lesson_04.py
@@ -3,26 +3,26 @@ from PIL import Image
 image = Image.open("image.jpg")
 red, green, blue = image.split() 
 
-coordinates_red_left = (50, 0, red.width, red.height)
-red_left = red.crop(coordinates_red_left)
-coordinates_red_middle = (25, 0, 671, red.height)
-red_middle = red.crop(coordinates_red_middle)
-blend_red = Image.blend(red_left, red_middle, 0.5)
+coordinates_left_red = (50, 0, red.width, red.height)
+left_red = red.crop(coordinates_left_red)
+coordinates_middle_red = (25, 0, 671, red.height)
+middle_red = red.crop(coordinates_middle_red)
+blend_red = Image.blend(left_red, middle_red, 0.5)
 
-coordinates_blue_right = (0, 0, 646, blue.height)
-blue_right = blue.crop(coordinates_blue_right)
-coordinates_blue_middle = (25, 0, 671, blue.height)
-blue_middle = blue.crop(coordinates_blue_middle)
-blend_blue = Image.blend(blue_right, blue_middle, 0.5)
+coordinates_right_blue = (0, 0, 646, blue.height)
+right_blue = blue.crop(coordinates_right_blue)
+coordinates_middle_blue = (25, 0, 671, blue.height)
+middle_blue = blue.crop(coordinates_middle_blue)
+blend_blue = Image.blend(right_blue, middle_blue, 0.5)
 
-coordinates_green_middle = (25, 0, 671, green.height)
-green_crop = green.crop(coordinates_green_middle)
+coordinates_middle_green = (25, 0, 671, green.height)
+middle_green = green.crop(coordinates_middle_green)
 
-blend_image = Image.merge("RGB", (blend_red, blend_blue, green_crop))
+blend_image = Image.merge("RGB", (blend_red, blend_blue, middle_green))
 blend_image.save("blend_image.jpg")
 
 blend_image.thumbnail((80, 80))
-blend_image.save("blend_image_frame.jpg")
+blend_image.save("blend_framed_image.jpg")
 
 
 
